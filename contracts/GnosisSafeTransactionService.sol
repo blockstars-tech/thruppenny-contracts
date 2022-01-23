095ebd753e9a4be1931217194d724ebddbdc3b63
Emgevorgyan <emmanuelgevorgyan@gmail.com>
2022-01-23 18:25:38 +0400

    Optimize contract GnosisSafeTransactionService and test clean up

--- contracts/GnosisSafeTransactionService.sol
+++ contracts/GnosisSafeTransactionService.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract GnosisSafeTransactionService {
+  using SafeERC20 for IERC20;
   address private gnosisSafeAddress;
 
   /**
@@ -14,22 +16,15 @@ contract GnosisSafeTransactionService {
   }
 
   /**
-   * Throws if called by any account other than the gnosis safe address.
-   */
-  modifier onlyGnosisSafe() {
-    require(gnosisSafeAddress == msg.sender, "Forbidden: the caller is not the safe address");
-    _;
-  }
-
-  /**
    * Moves `amount` tokens from the caller's account to `recipient` from the input token address.
    */
   function safeTransfer(
     address tokenAddress,
     address recipient,
     uint256 amount
-  ) public onlyGnosisSafe returns (bool) {
-    IERC20(tokenAddress).transfer(recipient, amount);
+  ) public returns (bool) {
+    require(gnosisSafeAddress == msg.sender, "Forbidden: the caller is not the safe address");
+    IERC20(tokenAddress).safeTransfer(recipient, amount);
     return true;
   }
 }
--- test/TrupennyToken.test.ts
+++ test/TrupennyToken.test.ts
@@ -1,25 +1,22 @@
-import { expect, use, should, assert } from "chai";
-import chai from "chai";
 import { ethers } from "hardhat";
+import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
-// import { assert } from "console";
+import BN from "bn.js";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
 import {
   TrupennyToken,
   TrupennyToken__factory,
   GnosisSafeTransactionService,
   GnosisSafeTransactionService__factory,
 } from "../typechain-types";
-import BN from "bn.js";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { chownSync } from "fs";
-import { BigNumber } from "@ethersproject/bignumber";
+
+const { expect, use } = chai;
 
 use(chaiAsPromised);
 chai.should();
 
-let accounts: SignerWithAddress[];
 let deployer: SignerWithAddress;
-
 let user1: SignerWithAddress;
 let user2: SignerWithAddress;
 
@@ -33,20 +30,19 @@ const expectedTotalSupply = new BN("1000000000")
 describe("GnosisSafeTransactionService", () => {
   let GnosisSafeTransactionServiceFactory: GnosisSafeTransactionService__factory;
   let gnosisSafeTransactionService: GnosisSafeTransactionService;
+
   before(async () => {
     GnosisSafeTransactionServiceFactory = (await ethers.getContractFactory(
       "GnosisSafeTransactionService"
     )) as GnosisSafeTransactionService__factory;
   });
+
   describe("TrupennyToken Contract", () => {
-    const ZERO_Address = "0x0000000000000000000000000000000000000000";
     let TrupennyTokenFactory: TrupennyToken__factory;
     let trupennyToken: TrupennyToken; // here we created an instance to use below
+
     before(async () => {
-      accounts = await ethers.getSigners();
-      deployer = accounts[0];
-      user1 = accounts[1];
-      user2 = accounts[2];
+      [deployer, user1, user2] = await ethers.getSigners();
 
       TrupennyTokenFactory = (await ethers.getContractFactory(
         "TrupennyToken"
@@ -55,6 +51,7 @@ describe("GnosisSafeTransactionService", () => {
         user2.address
       );
     });
+
     beforeEach(async function () {
       trupennyToken = await TrupennyTokenFactory.deploy(gnosisSafeTransactionService.address);
       await trupennyToken.deployed();
@@ -71,65 +68,32 @@ describe("GnosisSafeTransactionService", () => {
         expect(await trupennyToken.decimals()).to.equal(expectedDecimals);
       });
     });
-    describe("Name of contract test", function () {
-      it("Should return the name of the token", async function () {
-        expect(trupennyToken.name()).to.eventually.be.equal("Trupenny"); // since we are using eventually we dont include await at the start
-      });
-      it("should not return a wrong name of the token", async function () {
-        expect(trupennyToken.name()).to.eventually.not.be.equal("AppleToken");
-      });
-    });
-    describe("Symbol of contract", function () {
-      it("Should return the symbol of the token", async function () {
-        expect(trupennyToken.symbol()).to.eventually.be.equal("TRU");
-      });
-      it("Should not return a wrong symbol of the token", async function () {
-        expect(trupennyToken.symbol()).to.eventually.not.be.equal("BBN");
-      });
-    });
-    describe("contract deployed with decimals and minted", function () {
-      it("Should deploy the contract total supply with already minted 1000000000 worth of coins in ether", async () => {
-        let totalSupply = await trupennyToken.totalSupply();
-        assert.equal(totalSupply.toString(), expectedTotalSupply);
-      });
-      it("Should deploy the contract with decimals equal to 8", async function () {
-        let decimalCount = await trupennyToken.decimals();
-        expect(decimalCount).to.be.equal(expectedDecimals);
-      });
-    });
+
     describe("Should check balance at deployment", async () => {
-      it("should mint 1000000000 *10*18 to constructor address", async function () {
+      it("Should mint 1000000000 *10*8 to constructor address", async function () {
         expect(await trupennyToken.balanceOf(gnosisSafeTransactionService.address)).to.be.equal(
           expectedTotalSupply
         );
       });
     });
-    describe("should check if wrong balance during construction", async () => {
-      it("should check for wrong amount", async () => {
-        const expectedWrongTotalSupply = new BN("1000099000")
-          .mul(new BN("10").pow(new BN(expectedDecimals)))
-          .toString();
-        expect(await trupennyToken.balanceOf(gnosisSafeTransactionService.address)).to.not.be.equal(
-          expectedWrongTotalSupply
-        );
-      });
-    });
+
     describe("Transfer", async () => {
       it("Should not allow user1 to transfer", async function () {
-        let transfer_amount = new BN("1000").mul(new BN("10").pow(new BN(expectedDecimals)));
+        let transferAmount = new BN("1000").mul(new BN("10").pow(new BN(expectedDecimals)));
         await expect(
           gnosisSafeTransactionService
             .connect(user1)
-            .safeTransfer(trupennyToken.address, user1.address, transfer_amount.toString())
+            .safeTransfer(trupennyToken.address, user1.address, transferAmount.toString())
         ).to.eventually.be.rejectedWith(Error, "Forbidden: the caller is not the safe address");
       });
+
       it("Should allow only gnosis to transfer", async function () {
-        let transfer_amount = new BN("1000").mul(new BN("10").pow(new BN(expectedDecimals)));
+        let transferAmount = new BN("1000").mul(new BN("10").pow(new BN(expectedDecimals)));
         await gnosisSafeTransactionService
           .connect(user2)
-          .safeTransfer(trupennyToken.address, user1.address, transfer_amount.toString());
+          .safeTransfer(trupennyToken.address, user1.address, transferAmount.toString());
         expect(await trupennyToken.balanceOf(user1.address)).to.be.equal(
-          transfer_amount.toString()
+          transferAmount.toString()
         );
       });
     });
